38b558eebe89b83845e52f939d537f3114f6973a
developerSid <gary.w.myers@outlook.com>
2023-08-19 19:04:43 -0500

    Added remap for moving visually selected lines up and down

--- lua/remap.lua
+++ lua/remap.lua
@@ -3,6 +3,10 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
 
+-- Bellow keymaps allow for moving visually selected lines up and down, also handles indents
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
 vim.keymap.set({"n", "v"}, "<leader>y", [["+y]])
 vim.keymap.set("n", "<leader>Y", [["+Y]])
 
