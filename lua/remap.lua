858fb8d5967ab574c05cb6d235d88d2c80c142e3
developerSid <gary.w.myers@outlook.com>
2023-12-29 00:01:28 -0600

    Remapped delete in normal and visual mode to blackhole

--- lua/remap.lua
+++ lua/remap.lua
@@ -23,3 +23,10 @@ vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous dia
 vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
 vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Remap normal line delete to blackhole delete
+vim.api.nvim_set_keymap('n', 'dd', '"_dd', { noremap = true, silent = true })
+
+-- Remap visual delete to blackhole the deleted content
+vim.api.nvim_set_keymap('v', 'd', '"_d', { noremap = true, silent = true })
+
