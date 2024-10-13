7de87b40ab85053e9841d5264004e16ecbcfa698
developerSid <gary.w.myers@outlook.com>
2024-10-13 11:59:19 -0500

    Configured search all with telescope to ignore .git

--- lua/my/plugins/telescope.lua
+++ lua/my/plugins/telescope.lua
@@ -77,6 +77,9 @@ return { -- Fuzzy Finder (files, lsp, etc)
 		vim.keymap.set("n", "<leader>sr", builtin.resume, { desc = "[S]earch [R]esume" })
 		vim.keymap.set("n", "<leader>s.", builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
 		vim.keymap.set("n", "<leader><leader>", builtin.buffers, { desc = "[ ] Find existing buffers" })
+		vim.keymap.set("n", "<leader>sa", function()
+			builtin.find_files({ hidden = true, file_ignore_patterns = { ".git/" } })
+		end, { noremap = true, silent = true, desc = "[F]ind in [A]ll files" })
 
 		-- Slightly advanced example of overriding default behavior and theme
 		vim.keymap.set("n", "<leader>/", function()
